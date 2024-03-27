7eb09ba4a30b29aba1b6296b848cb75a2230e762
lursz <lukoszr@gmail.com>
2024-03-27 20:33:47 +0100

    Installed GUI
    chose GUI for the project: https://github.com/gui-cs/Terminal.Gui

--- MedicalClinic/Program.cs
+++ MedicalClinic/Program.cs
@@ -3,6 +3,8 @@ using MedicalClinic.DataBase;
 using MedicalClinic.DataBase.Connection;
 using MedicalClinic.DataBase.Models;
 using MedicalClinic.Exceptions;
+using MedicalClinic.UI;
+using Terminal.Gui;
 
 namespace MedicalClinic;
 
@@ -15,63 +17,95 @@ internal static class Program
         DbHandler.MigrateDB();
         var patient = new Patient();
 
-        var inputNum = 0;
-        do
-        {
-            Console.WriteLine("Patients database main menu");
-            Console.WriteLine("[1] List all customers");
-            Console.WriteLine("[2] Add random patient");
-            Console.WriteLine("[3] Add customer");
-            Console.WriteLine("[4] Edit customer");
-            Console.WriteLine("[5] Delete customer");
-            Console.WriteLine("[6] Search customers");
-            Console.WriteLine("[6] Clear database");
-            Console.WriteLine("[q] Quit");
+        
+        
+        Application.Run<MainWindow> ();
 
-            inputNum = Console.ReadKey().KeyChar;
-            Console.WriteLine(inputNum);
-            Console.WriteLine("\n");
+        System.Console.WriteLine ($"Username: {((MainWindow)Application.Top).usernameText.Text}");
 
-            try
-            {
-                switch (inputNum)
-                {
-                    case '1':
-                        PromptHub.ListAllPatients();
-                        break;
-                    case '2':
-                        PromptHub.AddRandomPatientPrompt();
-                        break;
-                    case '3':
-                        PromptHub.AddNewPatientPrompt();
-                        break;
-                    case '4':
-                        PromptHub.EditPatientPrompt();
-                        break;
-                    case '5':
-                        PromptHub.DeletePatientPrompt();
-                        break;
-                    case '6':
-                        PromptHub.SearchForPatient(patient);
-                        break;
-                    case 'c':
-                        DbHandler.Clear();
-                        break;
-                    case 'q':
-                        break;
-                    default:
-                        Console.WriteLine("Invalid input");
-                        break;
-                }
-            }
-            catch (BadIdException e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("An error occurred: " + e.Message);
-            }
-        } while (inputNum != 'q');
+        // Before the application exits, reset Terminal.Gui for clean shutdown
+        Application.Shutdown ();
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // var inputNum = 0;
+        // do
+        // {
+        //     Console.WriteLine("Patients database main menu");
+        //     Console.WriteLine("[1] List all customers");
+        //     Console.WriteLine("[2] Add random patient");
+        //     Console.WriteLine("[3] Add customer");
+        //     Console.WriteLine("[4] Edit customer");
+        //     Console.WriteLine("[5] Delete customer");
+        //     Console.WriteLine("[6] Search customers");
+        //     Console.WriteLine("[6] Clear database");
+        //     Console.WriteLine("[q] Quit");
+        //
+        //     inputNum = Console.ReadKey().KeyChar;
+        //     Console.WriteLine(inputNum);
+        //     Console.WriteLine("\n");
+        //
+        //     try
+        //     {
+        //         switch (inputNum)
+        //         {
+        //             case '1':
+        //                 PromptHub.ListAllPatients();
+        //                 break;
+        //             case '2':
+        //                 PromptHub.AddRandomPatientPrompt();
+        //                 break;
+        //             case '3':
+        //                 PromptHub.AddNewPatientPrompt();
+        //                 break;
+        //             case '4':
+        //                 PromptHub.EditPatientPrompt();
+        //                 break;
+        //             case '5':
+        //                 PromptHub.DeletePatientPrompt();
+        //                 break;
+        //             case '6':
+        //                 PromptHub.SearchForPatient(patient);
+        //                 break;
+        //             case 'c':
+        //                 DbHandler.Clear();
+        //                 break;
+        //             case 'q':
+        //                 break;
+        //             default:
+        //                 Console.WriteLine("Invalid input");
+        //                 break;
+        //         }
+        //     }
+        //     catch (BadIdException e)
+        //     {
+        //         Console.WriteLine(e.Message);
+        //     }
+        //     catch (Exception e)
+        //     {
+        //         Console.WriteLine("An error occurred: " + e.Message);
+        //     }
+        // } while (inputNum != 'q');
     }
 }
